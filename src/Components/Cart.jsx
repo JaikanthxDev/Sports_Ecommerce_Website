58e58a3c9917a8a8c7b87a3ff6792a3a57e3ebe7
Alraffi28 <mohammedalraffi@gmail.com>
2025-12-03 11:26:28 +0530

    styled cart

--- src/Components/Cart.jsx
+++ src/Components/Cart.jsx
@@ -1,25 +1,62 @@
 import React, { useContext } from 'react'
 import { CartContext } from '../Context/CartContext'
+import './Cricket.css'
+
+function cleanPrice(str){
+  if(!str) return 0
+
+const cleaned = String(str)
+    .replace(/MRP:/gi, "")   //  MRP
+    .replace(/₹/g, "")       //  ₹
+    .replace(/,/g, "")       
+    .replace(/\.00/g, "")    
+    .trim();
+
+    const number = parseFloat(cleaned);
+  return isNaN(number) ? 0 : number;
+}
 
 export default function Cart() {
 
-  const { cart } = useContext(CartContext)
+  const { cart , removeItem , increase , decrease } = useContext(CartContext)
+
+  const totalPrice = cart.reduce((total , item)=>{
+    const price = cleanPrice(item.price);
+    return total + price * item.qty
+  },0)
 
   return (
     <>
-    <div>
-      <h1>Your Cart</h1>
+    <h1 className='side-head'>Your Cart</h1>
+    <div className='cart-container'>
+      
       {
         cart.length === 0 ? (
           <p>Your Cart is Empty</p>
         ) : (
-          cart.map((item,index) => (
-            <div className='cart-item' key={index}>
-              <img src={item.image} alt="" />
-              <h3>{item.name}</h3>
-              <p>{item.price}</p>
+          cart.map((item,index) => {
+            const priceNum = cleanPrice(item.price)
+            return(
+              <div className='cart-card' key={index}>
+                <button className='remove-btn' onClick={()=>removeItem(item.name)}>✖</button>
+                <img src={item.image} alt="" />
+                <div className='cart-details'>
+              <h2>{item.name}</h2>
+              <p className='price'>₹{priceNum.toLocaleString('en-IN')} <span>{item.dis_price}</span></p>
+              <div className="qty-box">
+                <button className='qty-btn' onClick={() => decrease (item.name)}>-</button>
+                <span className='qty-number'>{item.qty}</span>
+                <button className='qty-btn' onClick={()=>increase(item.name)}>+</button>
+              </div>
+              </div>
             </div>
-          ))
+            )
+          })
+        )
+      }
+      {
+        cart.length > 0 && (
+          <h2 className='total-price'>Total : {totalPrice.toLocaleString('en-IN')}</h2>
         )
       }
     </div>
--- src/Context/CartContext.jsx
+++ src/Context/CartContext.jsx
@@ -5,10 +5,36 @@ export const CartContext = createContext()
 export default function CartProvider({children}) {
     const [cart ,setCart] = useState([]);
     function addToCart(product){
-        setCart(prev  => [...prev , product] )
+        setCart(prev  => {
+            const present = prev.find(item => item.name === product.name)
+            if(present){
+                return prev.map(item => item.name === product.name ?
+                    {...item , qty : item.qty + 1} 
+                    :  item
+                )
+            }
+            return [...prev , {...product , qty :1}]
+        })
+    }
+
+    function removeItem (name) {
+        setCart (prev => prev.filter(item => item.name !== name))
+    }
+
+    function increase (name){
+        setCart(prev => prev.map(
+            item => item.name === name ? {...item,qty:item.qty+1} : item
+        ))
+    }
+
+    function decrease (name){
+        setCart(prev => prev.map(
+            item => item.name === name && item.qty > 1 ? {...item,qty:item.qty-1}
+        : item
+    ))
     }
   return (
-    <CartContext.Provider value = {{cart , addToCart}}>
+    <CartContext.Provider value = {{cart , addToCart , removeItem ,increase , decrease}}>
         {children}
     </CartContext.Provider>
   )
